f35589fe34001abd5672eff2be7eb7d0a37d8b54
牛叠谊 <18715156450@163.com>
2022-03-11 18:50:01 +0800

    feat: add remix-theme to support theme change

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,78 @@
 import {
+  createCookieSessionStorage,
   Links,
+  LinksFunction,
   LiveReload,
+  LoaderFunction,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "remix";
 import type { MetaFunction } from "remix";
+import {createThemeSessionResolver,ThemeProvider, useTheme, PreventFlashOnWrongTheme} from 'remix-themes'
+import themeCss from './styles/theme.css'
+import globalCss from './styles/global.css'
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: 'remix-themes',
+    secure: true,
+    sameSite: 'lax',
+    secrets: ['s3cr3t'],
+    expires: new Date('2100-08-14'),
+    path: '/',
+    httpOnly: true,
+  },
+})
+
+export const themeSessionResolver = createThemeSessionResolver(sessionStorage)
+
+export const loader: LoaderFunction = async ({request}) => {
+  const {getTheme} = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
-export default function App() {
+
+export const links: LinksFunction = () => {
+  return [{rel: 'stylesheet', href: themeCss},{rel:'stylesheet',href: globalCss}]
+}
+
+
+export default function AppWithProviders() {
+  const data = useLoaderData()
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  )
+}
+
+function App() {
+  const data = useLoaderData()
+  const [theme] = useTheme()
+  return (
+    <html lang="en" className={theme ?? ''}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
-  );
+  )
 }
--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -1,9 +1,11 @@
 import styles from "~/styles/demo.css";
+import { Theme, useTheme } from 'remix-themes';
 
 export function links() {
     return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function Demo() {
-    return <div className="demo">demo</div>
+    const [_,setTheme] = useTheme();
+    return <div className="demo">demo<button onClick={() => setTheme(prev => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK))}>Toggle Theme</button></div>
 }
